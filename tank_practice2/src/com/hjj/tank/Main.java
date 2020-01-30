6d5c7bf87ab06a2c619c5f8a805836c0ae45d38c
hujunjie17607 <hujunjie17607@163.com>
2020-01-30 17:32:13 +0800

    认识Frame类

--- tank_practice2/src/com/hjj/tank/Main.java
+++ tank_practice2/src/com/hjj/tank/Main.java
@@ -1,9 +1,25 @@
 package com.hjj.tank;
 
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Frame f = new Frame();
+		f.setVisible(true);
+		f.setSize(800, 600);
+		f.setTitle("坦克大战");
+		f.setResizable(false);
+		f.addWindowListener(new WindowAdapter() {
 
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+
+		});
 	}
 
 }
